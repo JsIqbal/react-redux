c6c3f07b0b0f93a6696ca6b5860abbb5432444ba
JsIqbal <javascriptiqbal@gmail.com>
2022-10-31 18:17:27 +0600

    Task List Get/Post using Api

--- src/components/tasks/task-detail.js
+++ src/components/tasks/task-detail.js
@@ -1,13 +1,14 @@
 const TaskDetail = ({item, index}) => {
     return(
-        <tr key={item.id}>
-            <td>{index+1}</td>
-            <td>{item.email}</td>
-            <td>{item.description}</td>
-            <td>{item.priority}</td>
+        <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{item._id}</td>
+            <td>{item.Title}</td>
+            <td>{item.Description}</td>
+            <td>{item.Priority}</td>
             <td>
-                <button className='btn btn-circle'><i className="fa-solid fa-pen-to-square"></i></button>
-                <button className='btn btn-circle'><i className="fa-solid fa-delete-left"></i></button>
+                <button className='btn btn-circle btn-common'><i className="edit fa-solid fa-pen-to-square"></i></button>
+                <button className='btn btn-circle btn-common'><i className="delete fa-solid fa-delete-left"></i></button>
             </td>
         </tr>
     );
---
+++ src/services/task-service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export const getTaskData = async () => {
+    let data = [];
+    await axios.get("https://todo-app37.herokuapp.com/loadTodo")
+        .then(res => {
+            data = res.data;
+            // console.log("Data from Service >>", data);
+        });
+        return data;
+        
+}
+
+/**
+ * Store New Test Data to database
+ * 
+ * @param {object} taskItem 
+ */
+
+export const storeTaskData = async (taskItem) => {
+    let isAdded = false;
+    await axios.post("https://todo-app37.herokuapp.com/addTodo", taskItem)
+        .then(res => {
+            isAdded = res.data;
+        })
+        return isAdded;
+        
+}
